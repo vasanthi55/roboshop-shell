72f3cec2c8d70332008d9c9ecae25d2b96733f04
vasanthi55 <vasanthi.ankem@gmail.com>
2024-02-07 14:10:40 +0530

    install rabbitmq

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -16,7 +16,7 @@ echo "Script started exucuting at $TIMESTAMP" &>> $LOGFILE
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e "$2 .. $R installion failed $N"
+        echo -e "$2 .. $R installation failed $N"
         exit 1
     else
         echo -e "$2 .. $G installation success $N"
